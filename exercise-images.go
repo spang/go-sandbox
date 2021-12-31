b5ad3984b9b91bed25c006ea43484381e4260f60
Christine Spang <christine@spang.cc>
2021-12-31 16:09:29 -0500

    add images exercise

---
+++ exercise-images.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type Image struct{}
+
+func (i *Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i *Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 255, 255)
+}
+
+func (i *Image) At(x, y int) color.Color {
+	v := uint8(x * y)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(&m)
+}
